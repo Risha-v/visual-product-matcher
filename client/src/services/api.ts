324f31bc3303449b0fddbfb10e9ff7c1c92cdff8
Risha-v <120365272+Risha-v@users.noreply.github.com>
2026-02-14 16:03:08 +0530

    Update API_URL to use environment variable

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import { Product } from "../types";
 
-const API_URL = "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const searchProducts = async (
   imageData: string,
